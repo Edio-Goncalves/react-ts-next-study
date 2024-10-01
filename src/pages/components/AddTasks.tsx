93810b1549da101530492c6758d7114f7d76a0b4
Edio Goncalves Junior <ediogoncalvesjr@hotmail.com>
2024-10-01 13:47:02 -0300

    Create AddTasks style

--- src/pages/components/AddTasks.tsx
+++ src/pages/components/AddTasks.tsx
@@ -1,7 +1,9 @@
 export default function AddTasks() {
   return (
-    <div>
-      <p>Adiciona uma task</p>
+    <div className="addTasks-wrapper">
+      <input type="text" placeholder="Digite a tarefa" />
+      <input type="text" placeholder="Digite a descroção da tarefa" />
+      <button>Adicionar</button>
     </div>
   );
 }
